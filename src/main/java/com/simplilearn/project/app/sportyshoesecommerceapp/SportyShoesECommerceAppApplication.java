c330a40497662fa6a93c51092a92b0ffa0bbdf1a
Edson.Fufu <edson.fufu@nmbtz.com>
2022-05-17 21:59:37 +0300

    Initial Commit

---
+++ src/main/java/com/simplilearn/project/app/sportyshoesecommerceapp/SportyShoesECommerceAppApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.project.app.sportyshoesecommerceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportyShoesECommerceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportyShoesECommerceAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simplilearn/project/app/sportyshoesecommerceapp/SportyShoesECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.project.app.sportyshoesecommerceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyShoesECommerceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
